1da2b3667ced227f51a4f3716b717859610855eb
caolongyao <caolongyao@xywy.com>
2015-12-03 12:03:35 +0800

    add new project for this demo.

---
+++ PushTransitionDemo/PushTransitionDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PushTransitionDemo
+//
+//  Created by caolongyao on 15/12/3.
+//  Copyright © 2015年 iOSLittleSquad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
